0a35bf135b18cc7c7969715fcd4ec0fbe195ef04
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-05-10 17:34:26 +0900

    ステージ1>修正

--- client.py
+++ client.py
@@ -1,33 +1,62 @@
 import socket
+import threading
+import datetime
+
 IP_ADDR = '0.0.0.0'
 PORT = 9001
 MAX_BYTE = 4049
 
-# UNIXドメインソケットとデータグラム（非接続）ソケットを作成します
+server_addr  = (IP_ADDR, PORT)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-    try:
-        print('ユーザー名を入力してください')
-        username = input()
-        if username != '':
-            name_length = len(username)
-            username = str(name_length) + username
-            send_len = sock.sendto(username.encode('utf-8'), (IP_ADDR, PORT))
+# サーバーからのメッセージを受信
+def receive_messages():
+    while True:
+        
+        rx_data, addr = sock.recvfrom(MAX_BYTE)
+        
+        # ヘッダーからユーザー名長を取得
+        username_length = int.from_bytes(rx_data[:1], 'big')
+    
+        # ユーザー名とメッセージを分離
+        username = rx_data[1:username_length+1].decode('utf-8')
+        message = rx_data[username_length+1:].decode('utf-8')
+        
+        print(f"{username} > {message}")
 
-            print('Waiting response from server')
-            rx_message, addr = sock.recvfrom(MAX_BYTE)
-            print(f"[Server]:{rx_message.decode(encoding='utf-8')}")
-        else:
+# スレッドを開始
+receive_thread = threading.Thread(target=receive_messages, daemon=True)
+receive_thread.start()
+
+print('ユーザー名を入力してください')
+username = input()
+
+if username != '':
+    
+    while True:
+
+        try:
+            
+            message = input(' > ')
+            
+            # protocol_header()関数を用いてヘッダ情報を作成し、ユーザー名をヘッダーに設定
+            username_length = len(username.encode('utf-8'))
+            
+            # ヘッダーを作成
+            # to_bytes()メソッドを用いてバイナリに変換され、1つの64ビットバイナリに結合されます。
+            header = username_length.to_bytes(1, 'big') + username.encode('utf-8')
+                
+            # メッセージにヘッダーを付与する
+            data = header + message.encode('utf-8')                
+                
+            sock.sendto(data, server_addr)
+                        
+        except KeyboardInterrupt:
             print('closing socket')
             sock.close()
             print('done')
             break
         
-    except KeyboardInterrupt:
-        print('closing socket')
-        sock.close()
-        print('done')
-        break          
-
-
+else:
+    sock.close()
+    print('bye')
--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 import socket
-IP_ADDR = '0.0.0.0'
+import time
+
+# ブロードキャストするときは空文字
+IP_ADDR = ''
 PORT = 9001
 MAX_BYTE = 4049
+TIME_OUT = 60
+
+clients = []
 
 # udpで接続
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,30 +17,80 @@ print(f'接続開始します。{IP_ADDR}')
 sock.bind((IP_ADDR, PORT))
 
 try:
+
     while True:
         
         try:
+            
             # 一度に最大4096バイトを処理する
-            message, cli_addr = sock.recvfrom(MAX_BYTE)
+            data, addr = sock.recvfrom(MAX_BYTE)
             
-            client_data = message.decode(encoding='utf-8')
+            # ヘッダーからユーザー名長を取得
+            username_length = int.from_bytes(data[:1], 'big')
+    
+            # ユーザー名とメッセージを分離
+            username = data[1:username_length+1].decode('utf-8')
+            message = data[username_length+1:].decode('utf-8')
             
-            print('from username > ' + client_data, cli_addr)
+            # 現在時刻を取得
+            current_time = time.time()
             
-            if message:
-                response = 'hello > ' + client_data
-                sock.sendto(response.encode(encoding='utf-8'), cli_addr)
+            # メッセージの受信時間を保持
+            client_info = (addr, username, current_time)
+            
+            client_found = False
+            
+            # for i, client in enumerate(clients):
+            #     print('## > ', i,client)
+            #     print(client_info[:2])
+            #     print(client[:2])
+            #     if client[:2] == client_info[:2]:
+            #         clients[i] = client_info
+            #         client_found = True
+            #         break
+            # if not client_found:
+            #     clients.append(client_info)
+            #     print(f'New client connected: {username}')
+            
+            # 新規クライアントであれば追加
+            if addr not in clients:
+                clients.append(addr)
+            
+            print(f"> from {username} > {message}", addr)
+            
+            if data:
+                
+                # 接続中のクライアントに送信
+                for client in clients:
+                    
+                    # 最後の投稿から差分と現在時刻の差分を取得し、メッセージ送信対象か判定する
+                    
+                    
+                    # ヘッダーを作成
+                    username_length = len(username.encode('utf-8'))
+                    header = username_length.to_bytes(1, 'big') + username.encode('utf-8')
+                
+                    # メッセージにヘッダーを付与する
+                    data = header + message.encode('utf-8')                
+                
+                    c_addr, c_port = client
+                    
+                    sock.sendto(data, (c_addr, c_port))
+                    
             else:
                 print('no data from', IP_ADDR)
                 break
             
-        except Exception as e:
-            print('エラー > ' + str(e))
-            break
         except KeyboardInterrupt:
+            print('Interrupted.')
             sock.close()
             break
-                
+        
+        except socket.error:
+            print('Has error occurred.')
+            sock.close()
+            break
+        
 except Exception as ex:
     print(ex)
 finally:
