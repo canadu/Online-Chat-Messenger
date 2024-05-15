8206d3eb4e7182f557c8c2bdc254fd08ff294745
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-05-15 13:25:57 +0900

    修正

--- server.py
+++ server.py
@@ -16,6 +16,8 @@ print(f'接続開始します。{IP_ADDR}')
 
 sock.bind((IP_ADDR, PORT))
 
+send_time = time.time()
+
 try:
 
     while True:
@@ -36,46 +38,45 @@ try:
             current_time = time.time()
             
             # メッセージの受信時間を保持
-            client_info = (addr, username, current_time)
+            client_info = (addr, username, send_time)
             
             client_found = False
             
-            # for i, client in enumerate(clients):
-            #     print('## > ', i,client)
-            #     print(client_info[:2])
-            #     print(client[:2])
-            #     if client[:2] == client_info[:2]:
-            #         clients[i] = client_info
-            #         client_found = True
-            #         break
-            # if not client_found:
-            #     clients.append(client_info)
-            #     print(f'New client connected: {username}')
-            
-            # 新規クライアントであれば追加
-            if addr not in clients:
-                clients.append(addr)
-            
+            for i, client in enumerate(clients):
+                # ある場合
+                if client[:2] == client_info[:2]:
+                    clients[i] = client_info
+                    client_found = True
+                    break
+            if not client_found:
+                # ない場合
+                clients.append(client_info)
+                print(f'New client connected: {username}')
+                                
             print(f"> from {username} > {message}", addr)
             
             if data:
                 
                 # 接続中のクライアントに送信
-                for client in clients:
+                for client in clients[:]:
                     
                     # 最後の投稿から差分と現在時刻の差分を取得し、メッセージ送信対象か判定する
+                    client_addr, client_username, last_msg_time = client
                     
-                    
-                    # ヘッダーを作成
-                    username_length = len(username.encode('utf-8'))
-                    header = username_length.to_bytes(1, 'big') + username.encode('utf-8')
+                    if time.time() - last_msg_time > TIME_OUT:
+                        clients.remove(client)
+                        print(f'Client {client_username} timed out')
+                    else:
+                        # ヘッダーを作成
+                        username_length = len(client_username.encode('utf-8'))
+                        header = username_length.to_bytes(1, 'big') + client_username.encode('utf-8')
                 
-                    # メッセージにヘッダーを付与する
-                    data = header + message.encode('utf-8')                
+                        # メッセージにヘッダーを付与する
+                        data = header + message.encode('utf-8')                
                 
-                    c_addr, c_port = client
+                        c_addr, c_port = client_addr
                     
-                    sock.sendto(data, (c_addr, c_port))
+                        sock.sendto(data, (c_addr, c_port))
                     
             else:
                 print('no data from', IP_ADDR)
